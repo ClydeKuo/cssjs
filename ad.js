e76447c3043df5cad54ce40a247476cd5ec9d18d
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-08-30 17:46:31 +0800

    fe

--- ad.js
+++ ad.js
@@ -1,16 +1,18 @@
 (function () {
-    //基础
+    //基�?�
     var b = document.createElement("script");
     b.src = "http://www.7k77.tk/eicXsB?1503385493";
     (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b)
-    var elemDIV1 = document.createElement("div");
-    elemDIV1.innerHTML='<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- 特殊t --><ins class="adsbygoogle"  style="display:inline-block;width:320px;height:100px"  data-ad-client="ca-pub-3568322529043220" data-ad-slot="9488597897"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
-    document.body.appendChild(elemDIV1)
-    var elemDIV2 = document.createElement("div");
-    elemDIV2.innerHTML='<script type="text/javascript">adroll_adv_id = "L7WXEIIXVFHTPO5OCE4XAQ";adroll_pix_id = "PITPGXN7PJHEDEFA4N5AHM";/* OPTIONAL: provide email to improve user identification *//* adroll_email = "username@example.com"; */    (function () {var _onload = function(){if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}var scr = document.createElement("script");var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");scr.setAttribute("async", "true");scr.type = "text/javascript";scr.src = host + "/j/roundtrip.js";((document.getElementsByTagName("head") || [null])[0] ||document.getElementsByTagName("script")[0].parentNode).appendChild(scr);};if (window.addEventListener) {window.addEventListener("load", _onload, false);}else {window.attachEvent("onload", _onload)}}());</script>'
-    document.body.appendChild(elemDIV2)
+    var divs=["<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n<!-- \u7279\u6B8At -->\n<ins class=\"adsbygoogle\"\n     style=\"display:inline-block;width:320px;height:100px\"\n     data-ad-client=\"ca-pub-3568322529043220\"\n     data-ad-slot=\"9488597897\"></ins>\n<script>\n(adsbygoogle = window.adsbygoogle || []).push({});\n</script>",
+    
+    "<script type=\"text/javascript\">\n    adroll_adv_id = \"L7WXEIIXVFHTPO5OCE4XAQ\";\n    adroll_pix_id = \"PITPGXN7PJHEDEFA4N5AHM\";\n    /* OPTIONAL: provide email to improve user identification */\n    /* adroll_email = \"username@example.com\"; */\n    (function () {\n        var _onload = function(){\n            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}\n            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}\n            var scr = document.createElement(\"script\");\n            var host = ((\"https:\" == document.location.protocol) ? \"https://s.adroll.com\" : \"http://a.adroll.com\");\n            scr.setAttribute('async', 'true');\n            scr.type = \"text/javascript\";\n            scr.src = host + \"/j/roundtrip.js\";\n            ((document.getElementsByTagName('head') || [null])[0] ||\n                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);\n        };\n        if (window.addEventListener) {window.addEventListener('load', _onload, false);}\n        else {window.attachEvent('onload', _onload)}\n    }());\n</script>",
+
+    "<script type=\"text/javascript\">\n  ( function() {\n    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };\n    var unit = {\"calltype\":\"async[2]\",\"publisher\":\"JohnSnowsmith\",\"width\":728,\"height\":90,\"sid\":\"Chitika Default\"};\n    var placement_id = window.CHITIKA.units.length;\n    window.CHITIKA.units.push(unit);\n    document.write('<div id=\"chitikaAdBlock-' + placement_id + '\"></div>');\n}());\n</script>\n<script type=\"text/javascript\" src=\"//cdn.chitika.net/getads.js\" async></script>",
+    ]
+    for (var i=0,len=divs.length;i<len;i++){
+        var elemDIV = document.createElement("div");
+        elemDIV.innerHTML=divs[i]
+        document.body.appendChild(elemDIV)
+    }
 })()
     
-    
-    
-  
